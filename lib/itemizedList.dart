92c45141df0c2c8027fa679939f5071c05e4a073
Roman.Rojas <roman.rojas@suddath.com>
2020-07-22 08:47:24 -0400

    added new page info

--- lib/itemizedList.dart
+++ lib/itemizedList.dart
@@ -17,7 +17,7 @@ class ItemizedList extends StatelessWidget {
           Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Container(width: 100, child: Text("Account ")),
+              Container(width: 100, child: Text("Account 123")),
               Flexible(child: TextFormField(keyboardType: TextInputType.text))
             ],
           ),
