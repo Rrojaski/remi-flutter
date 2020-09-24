89dc34a6fca90df127967197037f130eed5695b7
R <roman.rojas@suddath.com>
2020-09-24 08:26:05 -0400

    passed state to child card 1

--- lib/screens/study/study.dart
+++ lib/screens/study/study.dart
@@ -3,6 +3,7 @@ import './card_1.dart';
 import './card_2.dart';
 import './card_definition.dart';
 import 'dart:math';
+import '../../models/Chinese_Card.model.dart';
 
 class StudyScreen extends StatefulWidget {
   @override
@@ -16,10 +17,24 @@ getRandomNumber() {
 }
 
 class StudyScreenState extends State<StudyScreen> {
+  List<ChineseCard> cardList = [
+    new ChineseCard(character: "水", meaning: "water", piyin: "shui"),
+    new ChineseCard(character: "頻果", meaning: "apple", piyin: "píngguǒ"),
+    new ChineseCard(character: "西瓜", meaning: "watermelon", piyin: "xīguā"),
+    new ChineseCard(character: "北京", meaning: "beijing", piyin: "pīn​yīn​"),
+    new ChineseCard(character: "再見", meaning: "goodbye", piyin: "zàijiàn"),
+    new ChineseCard(character: "後面", meaning: "behind", piyin: "hòumiàn"),
+    new ChineseCard(character: "家", meaning: "yesterday", piyin: "family"),
+    new ChineseCard(character: "熱", meaning: "hot", piyin: "rè")
+  ];
+
   @override
   Widget build(BuildContext context) {
     List<Widget> children = new List.generate(
-        1, (int i) => getRandomNumber() == 1 ? new Card1() : new Card2());
+        1,
+        (int i) => getRandomNumber() == 1
+            ? new Card1(chineseCard: cardList[0])
+            : new Card1(chineseCard: cardList[0]));
     return Scaffold(
         appBar: AppBar(
             iconTheme: IconThemeData(
@@ -31,7 +46,9 @@ class StudyScreenState extends State<StudyScreen> {
               style: TextStyle(fontSize: 20, color: Colors.white),
             )),
         body: Container(
+            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 30.0),
             width: double.infinity,
+            height: MediaQuery.of(context).size.height,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.center,
