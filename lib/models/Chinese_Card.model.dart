d14cab532ffdd81fe795b14801f5d4598f754863
R <roman.rojas@suddath.com>
2020-10-06 16:42:30 -0400

    added get cards call

--- lib/models/Chinese_Card.model.dart
+++ lib/models/Chinese_Card.model.dart
@@ -1,9 +1,9 @@
 class ChineseCard {
-  final int id = 0;
+  final int id;
   final String character;
   final String meaning;
   final String piyin;
   final String image;
 
-  ChineseCard({this.character, this.meaning, this.piyin, this.image});
+  ChineseCard({this.id, this.character, this.meaning, this.piyin, this.image});
 }
